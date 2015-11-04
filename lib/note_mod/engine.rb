b69ec4c5a5fd531b3819685590eef5a368811e8f
libochao <522427432@qq.com>
2015-11-04 10:28:18 +0800

    加了三行代码 在engine

--- lib/note_mod/engine.rb
+++ lib/note_mod/engine.rb
@@ -3,6 +3,9 @@ module NoteMod
     isolate_namespace NoteMod
     config.to_prepare do
       ApplicationController.helper ::ApplicationHelper
+      Dir.glob(Rails.root + "app/decorators/kc_courses/**/*_decorator.rb").each do |c|
+        require_dependency(c)
+      end
       User.class_eval do
         has_many :notes, class_name: 'NoteMod::Note'
       end
